7cbb028e72feed9e94a1f1460253cbe58f13c2a6
BE <BE@GAME>
2025-03-28 15:21:13 +0800

    *

--- main.go
+++ main.go
@@ -15,15 +15,15 @@ func main() {
 	wg := sync.WaitGroup{}
 	// 开 8 个携程去跑
 	for n := range 8 {
-		x := n
+
 		wg.Add(1)
-		go func() {
+		go func(workerID int) {
 			defer wg.Done()
 
-			i := int64(0)
+			cnt := int64(0)
 
 			for {
-				i++
+				cnt++
 
 				// 生成助记词
 				mn := mnemonic.Generate()
@@ -41,17 +41,21 @@ func main() {
 
 				// 检查是不是 XXXXYYYY
 				// I7-11700 CPU 90%+ 跑了8个小时，没跑出一个来
-				if last8[0] == last8[1] &&
-					last8[0] == last8[2] &&
-					last8[0] == last8[3] &&
-					last8[4] == last8[5] &&
-					last8[4] == last8[6] &&
-					last8[4] == last8[7] {
-					log.Printf("%v-%v Found: %v[%v]\n", x, i, addr, mn)
+				if checkPattern(last8) {
+					log.Printf("Worker(%v)- try(%v) Found addr: %v [%v]\n", workerID, cnt, addr, mn)
 				}
 			}
-		}()
+		}(n)
 	}
 
 	wg.Wait()
+}
+
+// checkPattern 检查字符串是否为XXXXYYYY格式
+func checkPattern(s string) bool {
+	if len(s) < 8 {
+		return false
+	}
+	return s[0] == s[1] && s[0] == s[2] && s[0] == s[3] &&
+		s[4] == s[5] && s[4] == s[6] && s[4] == s[7]
 }
